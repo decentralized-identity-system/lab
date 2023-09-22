3df7295f81d939c95c6e9050cccbd0231f0eb894
AlanRacciatti <alanracciatti1220@gmail.com>
2023-09-22 11:06:04 -0300

    feat: add ens support and feedback message on input

--- apps/decentralized-identity-app/components/xmtp/xmtp-start-conversation.tsx
+++ apps/decentralized-identity-app/components/xmtp/xmtp-start-conversation.tsx
@@ -1,7 +1,8 @@
 import { cn } from '@/lib/utils'
-import { useCanMessage, useSendMessage, useStartConversation } from '@xmtp/react-sdk'
+import { useCanMessage, useStartConversation, isValidAddress } from '@xmtp/react-sdk'
 import { useEffect, useState } from 'react'
 import { Button } from '../ui/button'
+import { useEnsAddress } from 'wagmi'
 
 type XMTPStartConversation = React.HTMLAttributes<HTMLElement> & {}
 
@@ -11,11 +12,29 @@ export const XMTPStartConversation = ({ className }: XMTPStartConversation) => {
   const [peerAddress, setPeerAddress] = useState('')
   const [message, setMessage] = useState('')
   const [peerIsActive, setPeerIsActive] = useState<boolean>(false)
+  const [canSendMessage, setCanSendMessage] = useState<boolean>(false)
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+
+  const checkAddress = async (address: string) => {
+    setIsLoading(true)
+    const isPeerActive = await canMessage(address)
+    setPeerIsActive(!!isPeerActive)
+    setIsLoading(false)
+  }
+
+  const {
+    data: ensResolvedAddress,
+    isLoading: isEnsLoading,
+    isFetching: isEnsFetching,
+  } = useEnsAddress({
+    name: peerAddress,
+  })
 
   const handleAccountChange = async (event: any) => {
     setPeerAddress(event.target.value)
-    const isPeerActive = await canMessage(event.target.value)
-    setPeerIsActive(!!isPeerActive)
+    if (isValidAddress(event.target.value)) {
+      checkAddress(event.target.value)
+    }
   }
 
   const handleMessageChange = (event: any) => {
@@ -31,7 +50,6 @@ export const XMTPStartConversation = ({ className }: XMTPStartConversation) => {
     }
   }
 
-  const [canSendMessage, setCanSendMessage] = useState<boolean>(false)
   useEffect(() => {
     if (peerIsActive && message.length > 0) {
       setCanSendMessage(true)
@@ -40,10 +58,40 @@ export const XMTPStartConversation = ({ className }: XMTPStartConversation) => {
     }
   }, [peerIsActive, message])
 
+  useEffect(() => {
+    if (ensResolvedAddress && isValidAddress(ensResolvedAddress)) checkAddress(ensResolvedAddress)
+  }, [ensResolvedAddress])
+
+  const getFeedbackMessage = () => {
+    if (isLoading || (peerAddress.endsWith('.eth') && (isEnsLoading || isEnsFetching))) {
+      return 'Finding address on the XMTP network'
+    }
+
+    if (peerAddress.length > 0 && !isValidAddress(peerAddress) && !peerAddress.endsWith('.eth')) {
+      return 'Please enter a valid 0x wallet, or ENS address'
+    }
+
+    if (!isLoading && !isEnsLoading && !isEnsFetching && !peerIsActive && (isValidAddress(peerAddress) || peerAddress.endsWith('.eth'))) {
+      return "Sorry, we can't message this address because its owner hasn't used it with XMTP yet"
+    }
+
+    return ''
+  }
+
   const classes = cn(className)
+
   return (
     <div className={classes}>
-      <input className="w-full input mb-4" type="text" placeholder="Account" value={peerAddress} onChange={handleAccountChange} />
+      <div className="mb-4">
+        <input
+          className="w-full input mb-1"
+          type="text"
+          placeholder="Enter an address or ENS..."
+          value={peerAddress}
+          onChange={handleAccountChange}
+        />
+        <p className="text-red-500 text-xs italic text-left">{getFeedbackMessage()}</p>
+      </div>
       <div className={'flex items-center gap-x-4'}>
         <input className="w-full input" type="text" placeholder="Type your message..." value={message} onChange={handleMessageChange} />
         <Button rounded={'lg'} disabled={!canSendMessage} variant={canSendMessage ? 'dark' : 'disabled'} onClick={handleStartConversation}>
